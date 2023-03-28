aca24c386d0ee42231396d59f040400c2107f804
Adewumi <owonikokoadewunmi@gmail.com>
2023-03-28 23:44:59 +0100

    adding to todo list

--- todo.js
+++ todo.js
@@ -3,40 +3,47 @@ const submitItem = document.getElementById("new-task-input");
 
 const tasks = document.getElementById("tasks");
 
-const todos= []
+const todos = [];
 
 const UI = (arr) => {
-let html = ""
-arr.forEach(todo => {
-    html +=`
+  let html = "";
+  arr.forEach((todo) => {
+    html += `
     <div id="task">
     <div class="content">
-      <p class="text"> my task</p>
+      <p class="text"> ${todo.title}</p>
       <!-- <input type="text" class="text" value="my task" /> -->
     </div>
     <div class="actions">
       <button class="edit">Edit</button>
       <button class="delete">Delete</button>
     </div>
-  </div>`
-
-});
-tasks.innerHTML = html;
+  </div>`;
+  });
+  tasks.innerHTML = html;
 };
+
 UI(todos);
+console.log(todos);
 
-submitItem.addEventListener('click', (e)=> {
-e.preventDefault()
+submitItem.addEventListener("click", (e) => {
+  e.preventDefault();
 
- const AddItem2 = AddItem.value
+  if(AddItem.value !== " "){
+    let currentTodo = {
+      title: AddItem.value,
+      id: Date.now(),
+      isComplete: false,
+    };
+  
+    todos.unshift(currentTodo);
+    console.log(todos);
+  
+    UI(todos);
+  
+    //clear input box after submitting
+    AddItem.value = " ";
 
- const displayItem = document.querySelector('.text')
-console.log(displayItem);
-
-    console.log(AddItem2)
-    todos.unshift(AddItem2)
-    console.log(todos)
-    displayItem.innerText += AddItem2
-    console.log(displayItem);
-}) 
-console.log(todos)
+  }
+});
+console.log(todos);
